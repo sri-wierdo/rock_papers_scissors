5c81aa93cb06a77a17abfb6ac453ab612070d1ac
sri_wierdo <mannemsrivishnureddy@gmail.com>
2022-05-04 12:30:02 +0530

    make user input case insensitive

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let computerScore = 0 ;
 //creating a function that prompts users to give input
 function getUserInput(){
  userInput = prompt("common...show me What you've got.... what do you wanna play:" );
+ userInput =  userInput.toLowerCase(); //this converts the input completely to lowercase so the program will work even if user input is in capitals
 }
 
 // creating a function that randomly picks either rock paper or scissor
@@ -17,79 +18,80 @@ function computerInput(){
     return inputCase[Math.round(Math.random() * (2))] 
 
 }
+
 /*here this is the actual logic of the game, this function takes userinput and compares it with computer's random choice and using a switch case we 
 return the result of the game  and also whenever someone gets a point we increment their score*/ 
 function play(userInput){
     let result = "" 
     if(userInput == "rock"){
         switch(computerInput()){
-        case "rock" :
-            return result = "womp womp... it's a tie nobody got a point"
-            break;
-        case "paper" :
-            computerScore++ ;
-            return result ="paper beats rock and you lost this"
-            break;
-        case "scissor" :
-            userScore++;
-            return result = "rock beats scissor my friend... and you got a point"
-            break;
+            case "rock" :
+                return result = "womp womp... it's a tie nobody got a point"
+                break;
+            case "paper" :
+                computerScore++ ;
+                return result ="paper beats rock and you lost this"
+                break;
+            case "scissor" :
+                userScore++;
+                return result = "rock beats scissor my friend... and you got a point"
+                break;
         }
 
 
 }   
-if(userInput == "paper"){
-        switch(computerInput()){
-        case "rock" :
-            userScore++ ;
-            return result = "paper beats rock my friend... and you got a point"
-            break;
-        case "paper" :
-
-            return result = "womp womp... it's a tie nobody got a point"
-            break;
-        case "scissor" :
-            computerScore++;
-            return result = "scissor cuts paper and you lost this round"
-            break;
-        }
-
-
-}
-if(userInput == "scissor"){
-        switch(computerInput()){
-        case "rock" :
-            computerScore++;
-            return result = "rock beats scissors and you lost this point"
-            break;
-        case "paper" :
-            userScore++;
-            return result = "scissors cut the paper down champion.. you've got a point"
-            break;
-        case "scissor" :
-            return result = "womp womp... it's a tie nobody got a point"
-            break;
-        }
+    if(userInput == "paper"){
+            switch(computerInput()){
+                case "rock" :
+                    userScore++ ;
+                    return result = "paper beats rock my friend... and you got a point"
+                    break;
+                case "paper" :
+                    return result = "womp womp... it's a tie nobody got a point"
+                    break;
+                case "scissor" :
+                    computerScore++;
+                    return result = "scissor cuts paper and you lost this round"
+                    break;
+            }
 
 
-} 
+    }
+    if(userInput == "scissor"){
+            switch(computerInput()){
+                case "rock" :
+                    computerScore++;
+                    return result = "rock beats scissors and you lost this point"
+                    break;
+                case "paper" :
+                    userScore++;
+                    return result = "scissors cut the paper down champion.. you've got a point"
+                    break;
+                case "scissor" :
+                    return result = "womp womp... it's a tie nobody got a point"
+                    break;
+            }
+
+
+    } 
 }
 /*in this function we are looping the code 5 times and at the end we are displaying who won the game*/
+
 function game(){
-for(let i = 0 ; i<5 ; i++){
-    getUserInput()
-    alert(play(userInput))
-}
+    for(let i = 0 ; i<5 ; i++){
+        getUserInput()
+        alert(play(userInput))
+    }
 
-if(userScore == computerScore){
-    alert("the game is a tie")
-}
-if(userScore > computerScore){
-    alert("you won this game")
-}
-if(userScore < computerScore){
-    alert("you lost this game")
-}
+    if(userScore == computerScore){
+        alert("the game is a tie")
+    }
+    if(userScore > computerScore){
+        alert("you won this game")
+    }
+    if(userScore < computerScore){
+        alert("you lost this game")
+    }
 
 }
-game() //game callback
+game();//game callback
